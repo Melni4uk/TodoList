1757b08c5e4b1e5fc3f79017394f5971572947ca
Николай <nick.melni4uk@gmail.com>
2018-07-13 17:29:14 +0300

    All work, without checkbox

--- Main.js
+++ Main.js
@@ -1,20 +1,14 @@
 const renderAll = () => {
 
-
-
     let todoKey = +localStorage.key(localStorage.length - 1);
     let header = document.createElement('div');
-
-    let todoContainer = document.getElementById('todos-Container');
     let deleteTodoButon = document.createElement('button');
     let toDo = document.createElement('span');
-    const todosContainer = document.createElement('div');
-    document.body.appendChild(todosContainer);
-
+    let todoContainer = document.createElement('div');
 
     function renderHeader() {
-
         document.body.appendChild(header);
+
         let inputToDo = document.createElement('input');
         header.appendChild(inputToDo);
         inputToDo.placeholder = 'Input todo';
@@ -27,58 +21,56 @@ const renderAll = () => {
             inputToDo.value = '';
         });
     }
-
     renderHeader();
 
     let mainContainer = document.createElement('div');
     document.body.appendChild(mainContainer);
-    function createToDos(text) {
 
+    function createToDos(text) {
 
         toDo.innerHTML = text;
 
-        if (todoContainer) {
+            mainContainer.appendChild(todoContainer);
             todoContainer.appendChild(toDo);
+
             todoKey += 1;
+            todoContainer.id = todoKey;
             localStorage.setItem(todoKey, toDo.innerHTML);
             toDo.id = todoKey;
 
-            toDo.appendChild(deleteTodoButon);
+            todoContainer.appendChild(deleteTodoButon);
             deleteTodoButon.innerHTML = 'Delete';
-        } else {
-
-
-            document.body.appendChild(todosContainer);
-            todosContainer.id = 'todos-Container';
-            todosContainer.appendChild(toDo);
-            todoKey += 1;
-            localStorage.setItem(todoKey, toDo.innerHTML);
-            toDo.id = todoKey;
-
-            todosContainer.appendChild(deleteTodoButon);
-            deleteTodoButon.innerHTML = 'Delete';
-        }
+            deleteTodoButon.name = 'buttonDelete';
 
     }
 
+
     getTodo = () => {
+        if (localStorage.length > 0){
+            for (let i = 1; i <= localStorage.length; i++) {
+                const todoContainerForGeted = document.createElement('div');
+                mainContainer.appendChild(todoContainerForGeted);
 
-        for (let i = 1; i <= localStorage.length; i++) {
-            const todoContainerForGeted = document.createElement('div');
-            mainContainer.appendChild(todoContainerForGeted);
-
-            let getedTodo = document.createElement('span');
-            getedTodo.innerHTML = localStorage.getItem(i);
-            todoContainerForGeted.appendChild(getedTodo);
-            getedTodo.id = i;
-
-            let deleteTodo = document.createElement('button');
-            todoContainerForGeted.appendChild(deleteTodo);
-            deleteTodo.innerHTML = 'Delete';
+                let getedTodo = document.createElement('span');
+                getedTodo.innerHTML = localStorage.getItem(i);
+                todoContainerForGeted.appendChild(getedTodo);
+                getedTodo.id = i;
+                todoContainerForGeted.id = i;
+                let deleteTodo = document.createElement('button');
+                todoContainerForGeted.appendChild(deleteTodo);
+                deleteTodo.innerHTML = 'Delete';
+                deleteTodo.name = 'buttonDelete';
+            }
         }
     }
 
     getTodo();
-
+    mainContainer.addEventListener("click", ({target}) =>{
+        if (target.name === 'buttonDelete'){
+            console.log('Done');
+            mainContainer.removeChild(target.parentNode);
+            localStorage.removeItem(target.parentNode.id);
+        }
+    })
 }
 renderAll();
