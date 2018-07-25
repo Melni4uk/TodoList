49a4c32b106ed35119cdf56b86009a4686c81da5
Николай <nick.melni4uk@gmail.com>
2018-07-25 15:47:28 +0300

    Done.

--- Main.js
+++ Main.js
@@ -1,127 +1,138 @@
-(renderAll = () => {
+(renderAll = () =>{
 
-    let header = document.createElement('div');
-
-    let todoObj = {};
-
-    (renderHeader = () => {
-
-        document.body.appendChild(header);
-
-        let inputToDo = document.createElement('input');
-        header.appendChild(inputToDo);
-        inputToDo.placeholder = 'Input todo';
-
-        let submitToDo = document.createElement('button');
-        header.appendChild(submitToDo);
-        submitToDo.innerHTML = 'Submit';
-        submitToDo.addEventListener('click', () => {
-            createToDos(inputToDo.value);
-            inputToDo.value = '';
-        });
-    })();
-
-    let mainContainer = document.createElement('div');
+    const mainContainer = document.createElement('div');
     document.body.appendChild(mainContainer);
+    mainContainer.className = 'mainContainer';
 
-    let todoKey = +localStorage.key(localStorage.length - 1);
+    const header = document.createElement('div');
+    mainContainer.appendChild(header);
 
-    let createToDos = (text) => {
+    const mainTodoContainer = document.createElement('div');
+    mainContainer.appendChild(mainTodoContainer);
 
-        let toDo = document.createElement('span');
+    let inputToDo = document.createElement('input');
+    header.appendChild(inputToDo);
+    inputToDo.placeholder = 'Input todo';
+    inputToDo.addEventListener('keyup', () =>{
+        if (inputToDo.value.length > 3) {
+            submitToDo.disabled = false;
+        } else {
+            submitToDo.disabled = true;
+        }
+    })
+
+    let submitToDo = document.createElement('button');
+    header.appendChild(submitToDo);
+    submitToDo.innerHTML = 'Submit';
+    submitToDo.disabled = true;
+    submitToDo.addEventListener('click', () => {
+            createNewTodoItem(inputToDo.value);
+            inputToDo.value = '';
+            submitToDo.disabled = true;
+    });
+
+    let uuid = () => {
+
+        let uuid = "", i, random;
+        for (i = 0; i < 32; i++) {
+            random = Math.random() * 16 | 0;
+
+            if (i == 8 || i == 12 || i == 16 || i == 20) {
+                uuid += "-"
+            }
+            uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
+        }
+        return uuid;
+    }
+
+    let showTodoItem = (parsedTodo) =>{
+
         let todoContainer = document.createElement('div');
+        mainTodoContainer.appendChild(todoContainer);
+        todoContainer.id = parsedTodo.id;
+        todoContainer.className = 'todoContainer';
+
         let checkbox = document.createElement('input');
         checkbox.setAttribute('type', 'checkbox');
-        todoKey += 1;
+        checkbox.checked = parsedTodo.checked;
         todoContainer.appendChild(checkbox);
-        checkbox.id = todoKey;
 
-        todoObj.value = text;
-        todoObj.id = todoKey;
-        todoObj.checked = false;
-        toDo.innerHTML = text;
+        let todoValue = document.createElement('span');
+        todoValue.innerHTML = parsedTodo.value;
+        todoContainer.appendChild(todoValue);
 
-        mainContainer.appendChild(todoContainer);
-        todoContainer.appendChild(toDo);
-
-        todoContainer.id = todoKey;
-        localStorage.setItem(todoKey, JSON.stringify(todoObj));
-        toDo.id = todoKey;
-
-        let deleteTodoButon = document.createElement('button');
-        todoContainer.appendChild(deleteTodoButon);
-        deleteTodoButon.innerHTML = 'Delete';
-        deleteTodoButon.name = 'buttonDelete';
+        let deleteTodo = document.createElement('button');
+        todoContainer.appendChild(deleteTodo);
+        deleteTodo.innerHTML = 'Delete';
+        deleteTodo.name = 'buttonDelete';
 
         let editTodoButton = document.createElement('button');
         todoContainer.appendChild(editTodoButton);
         editTodoButton.innerHTML = 'Edit todo';
         editTodoButton.name = 'buttonEdit';
+        editTodoButton.hidden = false;
 
-        let saveEdiedTodo = document.createElement('button');
-        todoContainer.appendChild(saveEdiedTodo);
-        saveEdiedTodo.innerHTML = 'Save changes';
-        saveEdiedTodo.name = 'buttonSave';
+        let saveEditedTodoButton = document.createElement('button');
+        todoContainer.appendChild(saveEditedTodoButton);
+        saveEditedTodoButton.innerHTML = 'Save changes';
+        saveEditedTodoButton.name = 'buttonSaveChange';
+        saveEditedTodoButton.hidden = true;
+
     }
 
-    let changeCheckboxValue = (id) => {
-        let parsedJSON = JSON.parse(localStorage.getItem(id));
+    let createNewTodoItem = (value) => {
+
+        let key = uuid();
+
+        let parsedTodo = {
+            'value' : value,
+            'checked' : false,
+            'id' : key
+        }
+        localStorage.setItem(key, JSON.stringify(parsedTodo));
+        showTodoItem(parsedTodo);
+    }
+
+    (getTodosFromLS = () =>{
+
+        Object.keys(localStorage).forEach(function (key){
+          let parsedItem =  JSON.parse(localStorage.getItem(key));
+          showTodoItem(parsedItem);
+        })
+    })()
+
+    let changeCheckboxValue = (key) => {
+
+        let parsedJSON = JSON.parse(localStorage.getItem(key));
         if (parsedJSON.checked === true){
             parsedJSON.checked = false;
         }
         else {
             parsedJSON.checked = true;
         }
-        localStorage.setItem(id, JSON.stringify(parsedJSON));
+        localStorage.setItem(key, JSON.stringify(parsedJSON));
     }
 
-    (getTodo = () => {
-        if (localStorage.length > 0){
-            Object.keys(localStorage).forEach(function (key){
-                const localStorageValue = JSON.parse(localStorage.getItem(key));
-                const todoItem = document.createElement('div');
-                mainContainer.appendChild(todoItem);
-                todoItem.id = key;
-                let checkbox = document.createElement('input');
-                checkbox.setAttribute('type', 'checkbox');
-                checkbox.checked = localStorageValue.checked;
+    mainTodoContainer.addEventListener("click", ({target}) =>{
 
-                todoItem.appendChild(checkbox);
-
-                let todoValue = document.createElement('span');
-                todoValue.innerHTML = localStorageValue.value;
-                todoItem.appendChild(todoValue);
-
-                let deleteTodo = document.createElement('button');
-                todoItem.appendChild(deleteTodo);
-                deleteTodo.innerHTML = 'Delete';
-                deleteTodo.name = 'buttonDelete';
-
-                let editTodoButton = document.createElement('button');
-                todoItem.appendChild(editTodoButton);
-                editTodoButton.innerHTML = 'Edit todo';
-                editTodoButton.name = 'buttonEdit';
-            })
-        }
-    })();
-
-    mainContainer.addEventListener("click", ({target}) =>{
         if (target.name === 'buttonDelete'){
             console.log('Done');
-            mainContainer.removeChild(target.parentNode);
+            mainTodoContainer.removeChild(target.parentNode);
             localStorage.removeItem(target.parentNode.id);
         }
     })
 
-    mainContainer.addEventListener("change", ({target}) =>{
+    mainTodoContainer.addEventListener("change", ({target}) =>{
+
         if (target.type === 'checkbox'){
             changeCheckboxValue(target.parentNode.id);
         }
     })
 
-    mainContainer.addEventListener("click", ({target}) =>{
-        if (target.innerHTML === 'Edit todo'){
-            /*mainContainer.removeChild(target.parent);*/
+    mainTodoContainer.addEventListener("click", ({target}) =>{
+
+        if (target.name === 'buttonEdit'){
+
             let editTodoInput = document.createElement('input');
             editTodoInput.placeholder = 'Input todo';
 
@@ -131,20 +142,28 @@
             editTodoInput.value = editingTodoValue;
             target.parentNode.removeChild(target.parentNode.firstChild.nextSibling);
             target.parentNode.insertBefore(editTodoInput, target.parentNode.firstChild.nextSibling);
-            target.innerHTML = 'Save changes';
+            target.hidden = true;
+            target.parentNode.lastChild.hidden = false;
 
             let editedTodo = document.createElement('span');
             editedTodo.innerHTML = editTodoInput.value;
-            if (target.parentNode.lastChild.innerHTML === 'Save changes'){
+
+            if (target.parentNode.lastChild.name === 'buttonSaveChange'){
+
                 target.parentNode.lastChild.addEventListener("click", ()=>{
-                    console.log('event');
+
                     editedTodo.innerHTML = editTodoInput.value;
                     target.parentNode.removeChild(target.parentNode.firstChild.nextSibling);
                     target.parentNode.insertBefore(editedTodo, target.parentNode.firstChild.nextSibling);
+                    target.hidden = false;
+                    target.parentNode.lastChild.hidden = true;
+                    
+                    let parsedJSON = JSON.parse(localStorage.getItem(target.parentNode.id));
+                    parsedJSON.value = editedTodo.innerHTML;
+                    localStorage.setItem(target.parentNode.id, JSON.stringify(parsedJSON));
                 })
 
             }
         }
     })
-
-})();
+})()
